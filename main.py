9e9b127cd9dda2a87fd1faecc94a41dbfe1d129b
Juan Felipe Garzon Baron <garzonbaronj@gmail.com>
2026-03-30 14:07:20 -0500

    lab 6.    7

---
+++ main.py
@@ -0,0 +1,45 @@
+from grades_manager import add_student, avg_by_student
+
+def main():
+    print("Welcome to the Student Grades Manager!\n")
+
+    my_grades = {}
+
+    while True:
+        print("Select an option:")
+        print("1. Add a student")
+        print("2. Print student grade averages")
+        print("3. Exit")
+
+        option = input()
+
+        if option == "1":
+            my_grades = add_student(my_grades)
+
+        elif option == "2":
+            print("Select an option:")
+            print("a. Display all students")
+            print("b. Display selected students")
+
+            sub_option = input()
+
+            if sub_option == "a":
+                avg_by_student(my_grades)
+
+            elif sub_option == "b":
+                names = input("Enter student names (comma-separated): ")
+                names_list = [name.strip() for name in names.split(",")]
+                avg_by_student(my_grades, names_list)
+
+            else:
+                print("Invalid option selected!")
+
+        elif option == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option selected!")
+
+if __name__ == "__main__":
+    main()
